5f43713e25a1bea5f10562723e2d6f44ae623fc8
Rob de Willigen <robdewilligen@outlook.com>
2023-03-28 16:33:57 +0200

    cleanup some code, add database json, cause all required data pulled from json.

--- src/js/bootstrap/router.js
+++ src/js/bootstrap/router.js
@@ -14,6 +14,7 @@ export default function (app) {
             path: "/",
             component: AppTemplate,
             redirect: { name: "Home" },
+            props: true,
             children: [
                 {
                     name: "Home",
